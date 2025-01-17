f020e280bbbbd8f5a7cf4253b17bf0f1e99fdb2d
dbaezh78 <108948897+dbaezh78@users.noreply.github.com>
2025-01-17 17:01:52 -0400

    update

---
+++ src/cjs/atisenorlevantomialma.js
@@ -0,0 +1,45 @@
+	// Titulo
+	let titulo = "A TI, SEÑOR, LEVANTO MI ALMA";
+	let salmo = "Salmo 25 (24)";
+
+    // Definir variables con la parte de la Asamble
+	
+	let Asamblea1 = "A TI, SEÑOR, LEVANTO MI ALMA,";
+	let Asamblea2 = "A TI, OH DIOS MÍO.";
+	let Asamblea3 = "A TI, SEÑOR, LEVANTO MI ALMA ...";
+	let Asamblea4 = "A TI, SEÑOR, LEVANTO MI ALMA ...";
+
+	// Definir variables con la parte del Solista
+	let Cantor1 = "En ti confío, Señor,";
+	let Cantor2 = "¡no triunfen sobre mí mis enemigos!";
+	let Cantor3 = "Muéstrame, Señor, tu camino,";
+	let Cantor4 = "enséñame, Señor, tu sendero.";
+	let Cantor5 = "Acuérdate, Señor, de tu ternura,";
+	let Cantor6 = "de tu inmensa compasión, que son de siempre.";
+	let Cantor7 = "De mis pecados, Señor, no te acuerdes,";
+	let Cantor8 = "acuérdate, Señor, que eres misericordia.";
+	let Cantor9 = "Rocíame, Señor, de tu misericordia.";
+	let Cantor10 = "Sumérgeme, Señor, en tu misericordia.";
+
+    // Asignando variable Tituto//
+    document.getElementById("t1").textContent = titulo;
+    document.getElementById("s1").textContent = salmo;
+
+    // Asignando variable para Asamble//
+	document.getElementById("a1").textContent = Asamblea1;
+	document.getElementById("a2").textContent = Asamblea2;
+	document.getElementById("a3").textContent = Asamblea3;
+	document.getElementById("a4").textContent = Asamblea4;
+	
+    // Asignando variable para Cantor o Solista//
+	document.getElementById("c1").textContent = Cantor1;
+	document.getElementById("c2").textContent = Cantor2;
+	document.getElementById("c3").textContent = Cantor3;
+	document.getElementById("c4").textContent = Cantor4;
+	document.getElementById("c5").textContent = Cantor5;
+	document.getElementById("c6").textContent = Cantor6;
+	document.getElementById("c7").textContent = Cantor7;
+	document.getElementById("c8").textContent = Cantor8;
+	document.getElementById("c9").textContent = Cantor9;
+	document.getElementById("c10").textContent = Cantor10;
+
