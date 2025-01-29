4dbd4e5517e40fdbcf66025c0a43c0a4a9a0740c
dbaezh78 <108948897+dbaezh78@users.noreply.github.com>
2025-01-29 16:52:57 -0400

    update

---
+++ src/cjs/aldespertar.js
@@ -0,0 +1,131 @@
+	// Titulo
+	let titulo = "AL DESPERTAR";
+	let salmo = "Salmo 17 (16)";
+	let dbnos = "29";	//Numero del libro
+
+    // Definir variables con la parte de la Asamble
+	
+	let Asamblea1 = "AL DESPERTAR";
+	let Asamblea2 = "ME SACIARÉ DE TU SEMBLANTE, SEÑOR.";
+	let Asamblea3 = "AL DESPERTAR";
+	let Asamblea4 = "ME SACIARÉ DE TU SEMBLANTE, SEÑOR. ";
+	let Asamblea5 = "AL DESPERTAR";
+	let Asamblea6 = "ME SACIARÉ DE TU SEMBLANTE, SEÑOR.";
+	let Asamblea7 = "AL DESPERTAR";
+	let Asamblea8 = "ME SACIARÉ DE TU SEMBLANTE, SEÑOR. ";
+	let Asamblea9 = "AL DESPERTAR";
+	let Asamblea10 = "ME SACIARÉ DE TU SEMBLANTE, SEÑOR.";
+	let Asamblea11 = "AL DESPERTAR";
+	let Asamblea12 = "ME SACIARÉ DE TU SEMBLANTE, SEÑOR. ";
+	let Asamblea13 = "AL DESPERTAR";
+	let Asamblea14 = "ME SACIARÉ DE TU SEMBLANTE, SEÑOR.";
+	let Asamblea15 = "AL DESPERTAR";
+	let Asamblea16 = "ME SACIARÉ DE TU SEMBLANTE, SEÑOR. ";
+	let Asamblea17 = "AL DESPERTAR";
+	let Asamblea18 = "ME SACIARÉ DE TU SEMBLANTE, SEÑOR.";
+	let Asamblea19 = "AL DESPERTAR";
+	let Asamblea20 = "ME SACIARÉ DE TU SEMBLANTE, SEÑOR. ";
+
+
+	// Definir variables con la parte del Solista
+	let Cantor1 = "Escucha, Señor, mi oración,";
+	let Cantor2 = "atiende, Señor, a mi clamor,";
+	let Cantor3 = "presta oídos a mi súplica,";
+	let Cantor4 = "que mis labios no traicionan, Señor.";
+	let Cantor5 = "Salga de ti la sentencia,";
+	let Cantor6 = "porque tus ojos ven, tus ojos ven mi rectitud.";
+	let Cantor7 = "Sondea, Señor, mi corazón,";
+	let Cantor8 = "escrútalo de noche, Señor;";
+	let Cantor9 = "pruébame al crisol, pruébame al crisol";
+	let Cantor10 = "porque tú sabes que no hay malicia en mí;";
+	let Cantor11 = "que mi boca no miente como hacen los hombres.";
+	let Cantor12 = "Que he guardado tu Palabra,";
+	let Cantor13 = "que no he seguido los caminos del violento,";
+	let Cantor14 = "que a tus huellas he ajustado mis pasos,";
+	let Cantor15 = "que no vacilaré, que no vacilaré.";
+	let Cantor16 = "A ti yo te invoco, Señor;";
+	let Cantor17 = "concédeme, Señor, una respuesta;";
+	let Cantor18 = "muéstrame los prodigios de tu amor,";
+	let Cantor19 = "porque tú salvas al que se apoya en ti,";
+	let Cantor20 = "porque tú ayudas";
+	let Cantor21 = "al que se refugia a tu derecha.";
+	let Cantor22 = "Como a la niña de tus ojos guárdame,";
+	let Cantor23 = "a la sombra de tus alas escóndeme,";
+	let Cantor24 = "mira que quieren matarme.";
+	let Cantor25 = "Míralos, están cerrados en su orgullo,";
+	let Cantor26 = "hablan, la arrogancia está en su boca.";
+	let Cantor27 = "Avanzan contra mí, avanzan contra mí.";
+	let Cantor28 = "¡Ayúdame tú, Señor,";
+	let Cantor29 = "líbrame de los hombres de este mundo,";
+	let Cantor30 = "de los hombres cuyo premio es esta vida!";
+	let Cantor31 = "¡Cólmalos de tus bienes, Señor,";
+	let Cantor32 = "llénales el vientre y que sus hijos se sacien!";
+	let Cantor33 = "Que yo al despertar, que yo por tu amor";
+	let Cantor34 = "me saciaré de tu rostro Señor.";
+
+
+
+    // Asignando variable Tituto//
+    document.getElementById("t1").textContent = titulo;
+    document.getElementById("s1").textContent = salmo;
+	document.getElementById("dbno").textContent = dbnos;
+
+    // Asignando variable para Asamble//
+	document.getElementById("a1").textContent = Asamblea1;
+	document.getElementById("a2").textContent = Asamblea2;
+	document.getElementById("a3").textContent = Asamblea3;
+	document.getElementById("a4").textContent = Asamblea4;
+	document.getElementById("a5").textContent = Asamblea5;
+	document.getElementById("a6").textContent = Asamblea6;
+	document.getElementById("a7").textContent = Asamblea7;
+	document.getElementById("a8").textContent = Asamblea8;
+	document.getElementById("a9").textContent = Asamblea9;
+	document.getElementById("a10").textContent = Asamblea10;
+	document.getElementById("a11").textContent = Asamblea11;
+	document.getElementById("a12").textContent = Asamblea12;
+	document.getElementById("a13").textContent = Asamblea13;
+	document.getElementById("a14").textContent = Asamblea14;
+	document.getElementById("a15").textContent = Asamblea15;
+	document.getElementById("a16").textContent = Asamblea16;
+	document.getElementById("a17").textContent = Asamblea17;
+	document.getElementById("a18").textContent = Asamblea18;
+	document.getElementById("a19").textContent = Asamblea19;
+	document.getElementById("a20").textContent = Asamblea20;
+
+	
+    // Asignando variable para Cantor o Solista//
+	document.getElementById("c1").textContent = Cantor1;
+	document.getElementById("c2").textContent = Cantor2;
+	document.getElementById("c3").textContent = Cantor3;
+	document.getElementById("c4").textContent = Cantor4;
+	document.getElementById("c5").textContent = Cantor5;
+	document.getElementById("c6").textContent = Cantor6;
+	document.getElementById("c7").textContent = Cantor7;
+	document.getElementById("c8").textContent = Cantor8;
+	document.getElementById("c9").textContent = Cantor9;
+	document.getElementById("c10").textContent = Cantor10;
+	document.getElementById("c11").textContent = Cantor11;
+	document.getElementById("c12").textContent = Cantor12;
+	document.getElementById("c13").textContent = Cantor13;
+	document.getElementById("c14").textContent = Cantor14;
+	document.getElementById("c15").textContent = Cantor15;
+	document.getElementById("c16").textContent = Cantor16;
+	document.getElementById("c17").textContent = Cantor17;
+	document.getElementById("c18").textContent = Cantor18;
+	document.getElementById("c19").textContent = Cantor19;
+	document.getElementById("c20").textContent = Cantor20;
+	document.getElementById("c21").textContent = Cantor21;
+	document.getElementById("c22").textContent = Cantor22;
+	document.getElementById("c23").textContent = Cantor23;
+	document.getElementById("c24").textContent = Cantor24;
+	document.getElementById("c25").textContent = Cantor25;
+	document.getElementById("c26").textContent = Cantor26;
+	document.getElementById("c27").textContent = Cantor27;
+	document.getElementById("c28").textContent = Cantor28;
+	document.getElementById("c29").textContent = Cantor29;
+	document.getElementById("c30").textContent = Cantor30;
+	document.getElementById("c31").textContent = Cantor31;
+	document.getElementById("c32").textContent = Cantor32;
+	document.getElementById("c33").textContent = Cantor33;
+	document.getElementById("c34").textContent = Cantor34;
+
