41d193de444f5153071969850972a746a4b17d4a
dbaezh78 <108948897+dbaezh78@users.noreply.github.com>
2025-03-31 17:52:29 -0400

    update

---
+++ src/cjs/aleluyaalabadalsenor.js
@@ -0,0 +1,59 @@
+// Titulo	
+let titulo = "ALELUYA, ALABAD AL SEÑOR";
+let salmo = "Salmo 150";
+let dbnos = "32";      //Numero del libro
+
+// Definir variables con la parte de la Asamble	
+let Asamblea1 = "¡ALELUYA, ALELUYA, ALELUYA!";
+let Asamblea2 = "¡ALELUYA, ALELUYA, ALELUYA!";
+let Asamblea3 = "¡ALELUYA, ALELUYA, ALELUYA!";
+let Asamblea4 = "¡ALELUYA, ALELUYA, ALELUYA!";
+let Asamblea5 = "¡ALELUYA, ALELUYA, ALELUYA!";
+let Asamblea6 = "¡ALELUYA, ALELUYA, ALELUYA!";
+let Asamblea7 = "¡TODO SER ALABE AL SEÑOR,";
+let Asamblea8 = "ALABE, ALABE AL SEÑOR!";
+let Asamblea9 = "¡ALELUYA, ALELUYA, ALELUYA!";
+let Asamblea10 = "¡ALELUYA, ALELUYA, ALELUYA!";
+
+
+// Definir variables con la parte de la Asamble	
+let Cantor1 = "Alabad al Señor en su templo,";
+let Cantor2 = "alabadlo en su fuerte firmamento,";
+let Cantor3 = "alabadlo por sus obras estupendas,";
+let Cantor4 = "alabadlo por su inmensa grandeza.";
+let Cantor5 = "Alabadlo al son de trompetas,";
+let Cantor6 = "alabadlo con arpas y guitarras,";
+let Cantor7 = "alabadlo con tambores y con danzas,";
+let Cantor8 = "alabadlo con trompas y flautas.";
+let Cantor9 = "Alabadlo con platillos sonoros,";
+let Cantor10 = "alabadlo con platillos vibrantes.";
+
+// Asignando variable Tituto//	
+document.getElementById("t1").textContent = titulo;
+document.getElementById("s1").textContent = salmo;
+document.getElementById("dbno").textContent = dbnos;
+
+// Asignando variable para Asamble//	
+document.getElementById("a1").textContent = Asamblea1;
+document.getElementById("a2").textContent = Asamblea2;
+document.getElementById("a3").textContent = Asamblea3;
+document.getElementById("a4").textContent = Asamblea4;
+document.getElementById("a5").textContent = Asamblea5;
+document.getElementById("a6").textContent = Asamblea6;
+document.getElementById("a7").textContent = Asamblea7;
+document.getElementById("a8").textContent = Asamblea8;
+document.getElementById("a9").textContent = Asamblea9;
+document.getElementById("a10").textContent = Asamblea10;
+
+// Asignando variable para Cantor o Solista//	
+document.getElementById("c1").textContent = Cantor1;
+document.getElementById("c2").textContent = Cantor2;
+document.getElementById("c3").textContent = Cantor3;
+document.getElementById("c4").textContent = Cantor4;
+document.getElementById("c5").textContent = Cantor5;
+document.getElementById("c6").textContent = Cantor6;
+document.getElementById("c7").textContent = Cantor7;
+document.getElementById("c8").textContent = Cantor8;
+document.getElementById("c9").textContent = Cantor9;
+document.getElementById("c10").textContent = Cantor10;
+// Fin del Codigo //	
---
+++ src/cjs/aleluyabendecidalsenor.js
@@ -0,0 +1,76 @@
+// Titulo	
+let titulo = "ALELUYA, BENDECID AL SEÑOR";
+let salmo = "Salmo 134 (133)";
+let dbnos = "33";      //Numero del libro
+
+// Definir variables con la parte de la Asamble	
+let Asamblea1 = "ALELÚ, ALELÚ, ALE E LU U YA,";
+let Asamblea2 = "ALELÚ, ALELÚ, ALE E LU U YA.";
+let Asamblea3 = "ALELUYA, ALELUYA, A A LE E LU U YA.";
+let Asamblea4 = "ALELUYA, ALELUYA, A A LE E LU U YA.";
+let Asamblea5 = "ALELÚ, ALELÚ, ALE E LU U YA,";
+let Asamblea6 = "ALELÚ, ALELÚ, ALE E LU U YA.";
+let Asamblea7 = "¡BENDECID AL SEÑOR, BENDECID AL SEÑOR";
+let Asamblea8 = "VOSOTROS LOS SIERVOS DEL SEÑOR,";
+let Asamblea9 = "BENDECID AL SEÑOR, BENDECID AL SEÑOR";
+let Asamblea10 = "VOSOTROS QUE ESTÁIS EN LA CASA DEL SEÑOR!";
+let Asamblea11 = "¡ALZAD A ÉL LAS MANOS,";
+let Asamblea12 = "ALZAD A ÉL LAS MANOS Y BENDECID AL SEÑOR,";
+let Asamblea13 = "VOSOTROS QUE ESTÁIS";
+let Asamblea14 = "EN LA CASA DEL SEÑOR DURANTE LA NOCHE!";
+let Asamblea15 = "ALELUYA, ALELUYA, A A LE E LU U YA.";
+let Asamblea16 = "ALELUYA, ALELUYA, A A LE E LU U YA.";
+let Asamblea17 = "ALELÚ, ALELÚ, ALE E LU U YA,";
+let Asamblea18 = "ALELÚ, ALELÚ, ALE E LU U YA.";
+
+
+// Definir variables con la parte de la Asamble	
+let Cantor1 = "¡Bendecid al Señor, bendecid al Señor";
+let Cantor2 = "vosotros los siervos del Señor,";
+let Cantor3 = "bendecid al Señor, bendecid al Señor";
+let Cantor4 = "vosotros que estáis en la casa del Señor!";
+let Cantor5 = "¡Alzad a él las manos,";
+let Cantor6 = "alzad a él las manos y bendecid al Señor,";
+let Cantor7 = "vosotros que estáis";
+let Cantor8 = "en la casa del Señor durante la noche!";
+let Cantor9 = "Aalelú, alelú uuu … uuya.";
+let Cantor10 = "(melisma)";
+
+
+// Asignando variable Tituto//	
+document.getElementById("t1").textContent = titulo;
+document.getElementById("s1").textContent = salmo;
+document.getElementById("dbno").textContent = dbnos;
+
+// Asignando variable para Asamble//	
+document.getElementById("a1").textContent = Asamblea1;
+document.getElementById("a2").textContent = Asamblea2;
+document.getElementById("a3").textContent = Asamblea3;
+document.getElementById("a4").textContent = Asamblea4;
+document.getElementById("a5").textContent = Asamblea5;
+document.getElementById("a6").textContent = Asamblea6;
+document.getElementById("a7").textContent = Asamblea7;
+document.getElementById("a8").textContent = Asamblea8;
+document.getElementById("a9").textContent = Asamblea9;
+document.getElementById("a10").textContent = Asamblea10;
+document.getElementById("a11").textContent = Asamblea11;
+document.getElementById("a12").textContent = Asamblea12;
+document.getElementById("a13").textContent = Asamblea13;
+document.getElementById("a14").textContent = Asamblea14;
+document.getElementById("a15").textContent = Asamblea15;
+document.getElementById("a16").textContent = Asamblea16;
+document.getElementById("a17").textContent = Asamblea17;
+document.getElementById("a18").textContent = Asamblea18;
+
+// Asignando variable para Cantor o Solista//	
+document.getElementById("c1").textContent = Cantor1;
+document.getElementById("c2").textContent = Cantor2;
+document.getElementById("c3").textContent = Cantor3;
+document.getElementById("c4").textContent = Cantor4;
+document.getElementById("c5").textContent = Cantor5;
+document.getElementById("c6").textContent = Cantor6;
+document.getElementById("c7").textContent = Cantor7;
+document.getElementById("c8").textContent = Cantor8;
+document.getElementById("c9").textContent = Cantor9;
+document.getElementById("c10").textContent = Cantor10;
+// Fin del Codigo //	
