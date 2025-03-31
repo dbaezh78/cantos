97c58bdafc1beea3a1658da6f31fc1a6c10f9f66
dbaezh78 <108948897+dbaezh78@users.noreply.github.com>
2025-03-30 20:29:33 -0400

    update

---
+++ src/cjs/ohmuertedondeestatuvictoria.js
@@ -0,0 +1,110 @@
+
+	// Titulo
+	
+	let titulo = "OH MUERTE ¿DÓNDE ESTÁ TU VICTORIA";
+	let salmo = "1ª Corintios 15";
+	let dbnos = "117";      //Numero del libro
+
+// Definir variables con la parte de la Asamble	
+	let Asamblea1 = "POR ESO CANTAMOS:";
+	let Asamblea2 = "LA MUERTE ES ABSORBIDA";
+	let Asamblea3 = "EN LA VICTORIA.";
+	let Asamblea4 = "ALELUYA, ALELUYA.";
+	let Asamblea5 = "OH MUERTE ¿DÓNDE ESTÁ TU VICTORIA?";
+	let Asamblea6 = "ALELUYA, ALELUYA.";
+	let Asamblea7 = "OH MUERTE ¿DÓNDE ESTÁ TU AGUIJÓN?";
+	let Asamblea8 = "ALELUYA, ALELUYA.";
+	let Asamblea9 = "POR ESO CANTAMOS:";
+	let Asamblea10 = "LA MUERTE ES ABSORBIDA";
+	let Asamblea11 = "EN LA VICTORIA.";
+	let Asamblea12 = "ALELUYA, ALELUYA.";
+	let Asamblea13 = "OH MUERTE ¿DÓNDE ESTÁ TU VICTORIA?";
+	let Asamblea14 = "ALELUYA, ALELUYA.";
+	let Asamblea15 = "OH MUERTE ¿DÓNDE ESTÁ TU AGUIJÓN?";
+	let Asamblea16 = "ALELUYA, ALELUYA.";
+	let Asamblea17 = "ALELUYA, ALELUYA, ALELUYA. ";
+	let Asamblea18 = "SEGÚN LAS ESCRITURAS;";
+	let Asamblea19 = "SEGÚN LAS ESCRITURAS; ";
+	let Asamblea20 = "POR ESO CANTAMOS:";
+	let Asamblea21 = "LA MUERTE ES ABSORBIDA";
+	let Asamblea22 = "EN LA VICTORIA.";
+	let Asamblea23 = "ALELUYA, ALELUYA.";
+	let Asamblea24 = "OH MUERTE ¿DÓNDE ESTÁ TU VICTORIA?";
+	let Asamblea25 = "ALELUYA, ALELUYA.";
+	let Asamblea26 = "OH MUERTE ¿DÓNDE ESTÁ TU AGUIJÓN?";
+	let Asamblea27 = "ALELUYA, ALELUYA.";
+	let Asamblea28 = "ALELUYA, ALELUYA, ALELUYA. ";
+
+	// Definir variables con la parte de la Asamble	
+	let Cantor1 = "En un instante, en un pestañear de ojos,";
+	let Cantor2 = "al toque de la trompeta final,";
+	let Cantor3 = "porque sonará la trompeta,";
+	let Cantor4 = "y los muertos resucitarán incorruptibles";
+	let Cantor5 = "y nosotros seremos transformados.";
+	let Cantor6 = "El aguijón de la muerte es el pecado;";
+	let Cantor7 = "y la fuerza del pecado, está en la Ley.";
+	let Cantor8 = "¡Pero nosotros vencemos en Cristo resucitado!";
+	let Cantor9 = "Yo os recuerdo, hermanos,";
+	let Cantor10 = "el Evangelio que yo os he anunciado,";
+	let Cantor11 = "el mismo que yo he recibido.";
+	let Cantor12 = "A saber:";
+	let Cantor13 = "que Cristo murió por los pecados,";
+	let Cantor14 = "que él ha resucitado";
+	let Cantor15 = "que se apareció primero a Pedro,";
+	let Cantor16 = "luego a los Doce; después a más";
+	let Cantor17 = "de quinientos hermanos a la vez.";
+	let Cantor18 = "Y por último se me apareció a mí.";
+
+    // Asignando variable js a variable html //
+    document.getElementById("t1").textContent = titulo;
+    document.getElementById("s1").textContent = salmo;
+	document.getElementById("dbno").textContent = dbnos;
+
+	document.getElementById("a1").textContent = Asamblea1;
+	document.getElementById("a2").textContent = Asamblea2;
+	document.getElementById("a3").textContent = Asamblea3;
+	document.getElementById("a4").textContent = Asamblea4;
+	document.getElementById("a5").textContent = Asamblea5;
+	document.getElementById("a6").textContent = Asamblea6;
+	document.getElementById("a7").textContent = Asamblea7;
+	document.getElementById("a8").textContent = Asamblea8;
+	document.getElementById("a9").textContent = Asamblea9;
+	document.getElementById("a10").textContent = Asamblea10;
+	document.getElementById("a11").textContent = Asamblea11;
+	document.getElementById("a12").textContent = Asamblea12;
+	document.getElementById("a13").textContent = Asamblea13;
+	document.getElementById("a14").textContent = Asamblea14;
+	document.getElementById("a15").textContent = Asamblea15;
+	document.getElementById("a16").textContent = Asamblea16;
+	document.getElementById("a17").textContent = Asamblea17;
+	document.getElementById("a18").textContent = Asamblea18;
+	document.getElementById("a19").textContent = Asamblea19;
+	document.getElementById("a20").textContent = Asamblea20;
+	document.getElementById("a21").textContent = Asamblea21;
+	document.getElementById("a22").textContent = Asamblea22;
+	document.getElementById("a23").textContent = Asamblea23;
+	document.getElementById("a24").textContent = Asamblea24;
+	document.getElementById("a25").textContent = Asamblea25;
+	document.getElementById("a26").textContent = Asamblea26;
+	document.getElementById("a27").textContent = Asamblea27;
+	document.getElementById("a28").textContent = Asamblea28;
+
+	document.getElementById("c1").textContent = Cantor1;
+	document.getElementById("c2").textContent = Cantor2;
+	document.getElementById("c3").textContent = Cantor3;
+	document.getElementById("c4").textContent = Cantor4;
+	document.getElementById("c5").textContent = Cantor5;
+	document.getElementById("c6").textContent = Cantor6;
+	document.getElementById("c7").textContent = Cantor7;
+	document.getElementById("c8").textContent = Cantor8;
+	document.getElementById("c9").textContent = Cantor9;
+	document.getElementById("c10").textContent = Cantor10;
+	document.getElementById("c11").textContent = Cantor11;
+	document.getElementById("c12").textContent = Cantor12;
+	document.getElementById("c13").textContent = Cantor13;
+	document.getElementById("c14").textContent = Cantor14;
+	document.getElementById("c15").textContent = Cantor15;
+	document.getElementById("c16").textContent = Cantor16;
+	document.getElementById("c17").textContent = Cantor17;
+	document.getElementById("c18").textContent = Cantor18;
+	
