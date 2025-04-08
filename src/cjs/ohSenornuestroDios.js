1d4b74584c077c9ebb552bb1cb0831f4c4570906
dbaezh78 <dbaezh78@gmail.com>
2025-04-07 23:11:42 -0400

    updateupdate

---
+++ src/cjs/ohSenornuestroDios.js
@@ -0,0 +1,62 @@
+	// Titulo	
+	let titulo = "OH SEÑOR, NUESTRO DIOS ";
+	let salmo = "Salmo 8";
+	let dbnos = "118";      //Numero del libro
+
+	// Definir variables con la parte de la Asamble	
+	let Asamblea1 = "¡OH SEÑOR, NUESTRO DIOS,";
+	let Asamblea2 = "QUÉ ADMIRABLE ES TU NOMBRE";
+	let Asamblea3 = "POR TODA LA TIERRA, TU NOMBRE,";
+	let Asamblea4 = "HASTA EL CIELO SE ELEVA TU AMOR!";
+	let Asamblea5 = "¡OH SEÑOR, NUESTRO DIOS,";
+	let Asamblea6 = "QUÉ ADMIRABLE ES TU NOMBRE";
+	let Asamblea7 = "POR TODA LA TIERRA, TU NOMBRE,";
+	let Asamblea8 = "HASTA EL CIELO SE ELEVA TU AMOR!";
+	let Asamblea9 = "¡OH SEÑOR, NUESTRO DIOS,";
+	let Asamblea10 = "QUÉ ADMIRABLE ES TU NOMBRE";
+	let Asamblea11 = "POR TODA LA TIERRA, TU NOMBRE,";
+	let Asamblea12 = "HASTA EL CIELO SE ELEVA TU AMOR!";
+
+	// Definir variables con la parte de la Asamble	
+	let Cantor1 = "Con la boca de los niños pequeños";
+	let Cantor2 = "afirmas tu gloria, oh Señor,";
+	let Cantor3 = "y reduces al silencio enemigos y rebeldes.";
+	let Cantor4 = "Si contemplo el cielo, obra de tus manos,";
+	let Cantor5 = "la luna y las estrellas, que has creado,";
+	let Cantor6 = "¿qué es el hombre para que te acuerdes de él,";
+	let Cantor7 = "el hijo del hombre para darle poder?";
+	let Cantor8 = "Lo hiciste, lo hiciste poco menos que los ángeles,";
+	let Cantor9 = "de gloria y honor lo has coronado;";
+	let Cantor10 = "todo lo has sometido bajo sus pies.";
+
+
+    // Asignando variable Tituto//
+    document.getElementById("t1").textContent = titulo;
+    document.getElementById("s1").textContent = salmo;
+	document.getElementById("dbno").textContent = dbnos;
+
+    // Asignando variable para Asamble//
+	document.getElementById("a1").textContent = Asamblea1;
+	document.getElementById("a2").textContent = Asamblea2;
+	document.getElementById("a3").textContent = Asamblea3;
+	document.getElementById("a4").textContent = Asamblea4;
+	document.getElementById("a5").textContent = Asamblea5;
+	document.getElementById("a6").textContent = Asamblea6;
+	document.getElementById("a7").textContent = Asamblea7;
+	document.getElementById("a8").textContent = Asamblea8;
+	document.getElementById("a9").textContent = Asamblea9;
+	document.getElementById("a10").textContent = Asamblea10;
+	document.getElementById("a11").textContent = Asamblea11;
+	document.getElementById("a12").textContent = Asamblea12;
+	
+    // Asignando variable para Cantor o Solista//
+	document.getElementById("c1").textContent = Cantor1;
+	document.getElementById("c2").textContent = Cantor2;
+	document.getElementById("c3").textContent = Cantor3;
+	document.getElementById("c4").textContent = Cantor4;
+	document.getElementById("c5").textContent = Cantor5;
+	document.getElementById("c6").textContent = Cantor6;
+	document.getElementById("c7").textContent = Cantor7;
+	document.getElementById("c8").textContent = Cantor8;
+	document.getElementById("c9").textContent = Cantor9;
+	document.getElementById("c10").textContent = Cantor10;
