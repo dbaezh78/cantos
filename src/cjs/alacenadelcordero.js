0c462432ad5c44ed7278beeb1ec19cf7fac66f28
dbaezh78 <dbaezh78@gmail.com>
2025-04-12 22:59:42 -0400

    update

---
+++ src/cjs/alacenadelcordero.js
@@ -0,0 +1,84 @@
+
+
+
+// Titulo	
+	let titulo = "A LA CENA DEL CORDERO ";
+	let salmo = "Himno lat. «Ad cenam Agni provldl";
+	let dbnos = "237";      //Numero del libro
+	
+// Definir variables con la parte de la Asamble	
+	let Asamblea1 = "A LA CENA DEL CORDERO,";
+	let Asamblea2 = "VESTIDOS CON BLANCAS TÚNICAS,";
+	let Asamblea3 = "ATRAVESADO EL MAR ROJO,";
+	let Asamblea4 = "CANTAMOS A CRISTO, EL SEÑOR";
+	let Asamblea5 = "A LA CENA DEL CORDERO,";
+	let Asamblea6 = "VESTIDOS CON BLANCAS TÚNICAS,";
+	let Asamblea7 = "ATRAVESADO EL MAR ROJO,";
+	let Asamblea8 = "CANTAMOS A CRISTO, EL SEÑOR";
+	let Asamblea9 = "A LA CENA DEL CORDERO,";
+	let Asamblea10 = "VESTIDOS CON BLANCAS TÚNICAS,";
+	let Asamblea11 = "ATRAVESADO EL MAR ROJO,";
+	let Asamblea12 = "CANTAMOS A CRISTO, EL SEÑOR";
+	let Asamblea13 = "AMÉN.";
+
+// Definir variables con la parte de la Asamble	
+	let Cantor1 = "Su cuerpo lleno de amor,";
+	let Cantor2 = "sobre la mesa es el Pan vivo.";
+	let Cantor3 = "Y su sangre sobre el altar,";
+	let Cantor4 = "es el cáliz del nuevo pacto.";
+	let Cantor5 = "Manso cordero inmolado,";
+	let Cantor6 = "es Cristo, nuestra Pascua.";
+	let Cantor7 = "Su cuerpo adorable";
+	let Cantor8 = "es el verdadero pan ácimo";
+	let Cantor9 = "En esta hora admirable";
+	let Cantor10 = "retornan los antiguos prodigios,";
+	let Cantor11 = "su brazo potente nos salva";
+	let Cantor12 = "del ángel de la muerte";
+	let Cantor13 = "Irradia sobre la Iglesia";
+	let Cantor14 = "el gozo pascual, oh Señor,";
+	let Cantor15 = "y une a tu gran victoria";
+	let Cantor16 = "a los que han renovado el Bautismo.";
+	let Cantor17 = "Sea honor, alabanza y gloria,";
+	let Cantor18 = "a Cristo victorioso de la muerte,";
+	let Cantor19 = "al Padre y al Santo Espíritu";
+	let Cantor20 = "ahora y por los siglos eternos.";
+
+    // Asignando variable js a variable html //
+    document.getElementById("t1").textContent = titulo;
+    document.getElementById("s1").textContent = salmo;
+	document.getElementById("dbno").textContent = dbnos;
+
+    document.getElementById("a1").textContent = Asamblea1;
+    document.getElementById("a2").textContent = Asamblea2;
+    document.getElementById("a3").textContent = Asamblea3;
+    document.getElementById("a4").textContent = Asamblea4;
+    document.getElementById("a5").textContent = Asamblea5;
+    document.getElementById("a6").textContent = Asamblea6;
+    document.getElementById("a7").textContent = Asamblea7;
+    document.getElementById("a8").textContent = Asamblea8;
+    document.getElementById("a9").textContent = Asamblea9;
+    document.getElementById("a10").textContent = Asamblea10;
+	document.getElementById("a11").textContent = Asamblea11;
+	document.getElementById("a12").textContent = Asamblea12;
+	document.getElementById("a13").textContent = Asamblea13;
+
+	document.getElementById("c1").textContent = Cantor1;
+	document.getElementById("c2").textContent = Cantor2;
+	document.getElementById("c3").textContent = Cantor3;
+	document.getElementById("c4").textContent = Cantor4;
+	document.getElementById("c5").textContent = Cantor5;
+	document.getElementById("c6").textContent = Cantor6;
+	document.getElementById("c7").textContent = Cantor7;
+	document.getElementById("c8").textContent = Cantor8;
+	document.getElementById("c9").textContent = Cantor9;
+	document.getElementById("c10").textContent = Cantor10;
+	document.getElementById("c11").textContent = Cantor11;
+	document.getElementById("c12").textContent = Cantor12;
+	document.getElementById("c13").textContent = Cantor13;
+	document.getElementById("c14").textContent = Cantor14;
+	document.getElementById("c15").textContent = Cantor15;
+	document.getElementById("c16").textContent = Cantor16;
+	document.getElementById("c17").textContent = Cantor17;
+	document.getElementById("c18").textContent = Cantor18;
+	document.getElementById("c19").textContent = Cantor19;
+	document.getElementById("c20").textContent = Cantor20;
