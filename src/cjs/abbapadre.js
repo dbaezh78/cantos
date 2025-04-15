f966139aa359f675c81ff917712257db3fa7ba3f
dbaezh78 <108948897+dbaezh78@users.noreply.github.com>
2025-04-15 18:40:06 -0400

    update

---
+++ src/cjs/abbapadre.js
@@ -0,0 +1,49 @@
+		// Titulo	
+		let titulo = "ABBÁ, PADRE";
+		let salmo = "Romanos 8,15-17";
+		let dbnos = "7";      //Numero del libro
+
+		// Definir variables con la parte de la Asamble	
+		let Asamblea1 = "¡ABBÁ, PADRE, PADRE!";
+		let Asamblea2 = "ESTE MISMO ESPÍRITU";
+		let Asamblea3 = "DA TESTIMONIO A NUESTRO ESPÍRITU";
+		let Asamblea4 = "DE QUE SOMOS HIJOS DE DIOS.";
+		let Asamblea5 = "Y, SI HIJOS, TAMBIÉN HEREDEROS,";
+		let Asamblea6 = "COHEREDEROS DE CRISTO,";
+		let Asamblea7 = "HEREDEROS DE DIOS.";
+		let Asamblea8 = "Y, SI HIJOS, TAMBIÉN HEREDEROS,";
+		let Asamblea9 = "COHEREDEROS DE CRISTO,";
+		let Asamblea10 = "HEREDEROS DE DIOS.";
+		let Asamblea11 = "¡ABBÁ, PADRE, PADRE!";
+
+		// Definir variables con la parte de la Asamble	
+		let Cantor1 = "No hemos recibido";
+		let Cantor2 = "el espíritu de siervos";
+		let Cantor3 = "para recaer en el temor,";
+		let Cantor4 = "para recaer en el temor.";
+
+		// Asignando variable Tituto//	
+		document.getElementById("t1").textContent = titulo;
+		document.getElementById("s1").textContent = salmo;
+		document.getElementById("dbno").textContent = dbnos;
+
+		// Asignando variable para Asamble//	
+		document.getElementById("a1").textContent = Asamblea1;
+		document.getElementById("a2").textContent = Asamblea2;
+		document.getElementById("a3").textContent = Asamblea3;
+		document.getElementById("a4").textContent = Asamblea4;
+		document.getElementById("a5").textContent = Asamblea5;
+		document.getElementById("a6").textContent = Asamblea6;
+		document.getElementById("a7").textContent = Asamblea7;
+		document.getElementById("a8").textContent = Asamblea8;
+		document.getElementById("a9").textContent = Asamblea9;
+		document.getElementById("a10").textContent = Asamblea10;
+		document.getElementById("a11").textContent = Asamblea11;
+
+		// Asignando variable para Cantor o Solista//	
+		document.getElementById("c1").textContent = Cantor1;
+		document.getElementById("c2").textContent = Cantor2;
+		document.getElementById("c3").textContent = Cantor3;
+		document.getElementById("c4").textContent = Cantor4;
+
+		// Fin del Codigo //	
