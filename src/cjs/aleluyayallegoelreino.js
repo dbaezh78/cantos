e00315e69255f0ecbcf101fdf70f6d2561b7e119
dbaezh78 <dbaezh78@gmail.com>
2025-04-17 00:11:53 -0400

    update

---
+++ src/cjs/aleluyayallegoelreino.js
@@ -0,0 +1,63 @@
+        // Titulo	
+        let titulo = "ALELUYA, YA LLEGÓ EL REINO ";
+        let salmo = "Apocalipsis 19,6-9";
+        let dbnos = "15";      //Numero del libro
+
+        // Definir variables con la parte de la Asamble	
+        let Asamblea1 = "ALELUYA,ALELUYA,ALELUYA.";
+        let Asamblea2 = "ALELUYA,ALELUYA,ALELUYA.";
+        let Asamblea3 = "ALELUYA,ALELUYA,ALELUYA.";
+        let Asamblea4 = "ALELUYA,ALELUYA,ALELUYA.";
+        let Asamblea5 = "ALELUYA,ALELUYA,ALELUYA.";
+        let Asamblea6 = "ALELUYA,ALELUYA,ALELUYA.";
+        let Asamblea7 = "ALELUYA,ALELUYA,ALELUYA.";
+        let Asamblea8 = "ALELUYA,ALELUYA,ALELUYA.";
+
+        // Definir variables con la parte de la Asamble	
+        let Cantor1 = "Ya llegó el Reino";
+        let Cantor2 = "del Señor y su Cristo.";
+        let Cantor3 = "Ya llegó el Reino";
+        let Cantor4 = "del Señor nuestro Dios.";
+        let Cantor5 = "Démosle gloria,";
+        let Cantor6 = "han llegado las bodas.";
+        let Cantor7 = "Démosle gloria,";
+        let Cantor8 = "las bodas con nuestro Dios.";
+        let Cantor9 = "Bienaventurados";
+        let Cantor10 = "los invitados a bodas,";
+        let Cantor11 = "a las bodas del Cordero.";
+        let Cantor12 = "Bienaventurados";
+        let Cantor13 = "los invitados a bodas,";
+        let Cantor14 = "a las bodas con el Señor.";
+
+
+        // Asignando variable Tituto//	
+        document.getElementById("t1").textContent = titulo;
+        document.getElementById("s1").textContent = salmo;
+        document.getElementById("dbno").textContent = dbnos;
+
+        // Asignando variable para Asamble//	
+        document.getElementById("a1").textContent = Asamblea1;
+        document.getElementById("a2").textContent = Asamblea2;
+        document.getElementById("a3").textContent = Asamblea3;
+        document.getElementById("a4").textContent = Asamblea4;
+        document.getElementById("a5").textContent = Asamblea5;
+        document.getElementById("a6").textContent = Asamblea6;
+        document.getElementById("a7").textContent = Asamblea7;
+        document.getElementById("a8").textContent = Asamblea8;
+
+        // Asignando variable para Cantor o Solista//	
+        document.getElementById("c1").textContent = Cantor1;
+        document.getElementById("c2").textContent = Cantor2;
+        document.getElementById("c3").textContent = Cantor3;
+        document.getElementById("c4").textContent = Cantor4;
+        document.getElementById("c5").textContent = Cantor5;
+        document.getElementById("c6").textContent = Cantor6;
+        document.getElementById("c7").textContent = Cantor7;
+        document.getElementById("c8").textContent = Cantor8;
+        document.getElementById("c9").textContent = Cantor9;
+        document.getElementById("c10").textContent = Cantor10;
+        document.getElementById("c11").textContent = Cantor11;
+        document.getElementById("c12").textContent = Cantor12;
+        document.getElementById("c13").textContent = Cantor13;
+        document.getElementById("c14").textContent = Cantor14;
+        // Fin del Codigo //	
