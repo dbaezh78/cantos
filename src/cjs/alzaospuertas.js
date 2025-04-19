fae8f52a707dfb3650941aeb25c0fa4be61acb55
dbaezh78 <dbaezh78@gmail.com>
2025-04-18 23:01:17 -0400

    update

---
+++ src/cjs/alzaospuertas.js
@@ -0,0 +1,105 @@
+        // Titulo	
+        let titulo = "ALZAOS,PUERTAS ";
+        let salmo = "Salmo 24 (23) ";
+        let dbnos = "16";      //Numero del libro
+
+        // Definir variables con la parte de la Asamble	
+        let Asamblea1 = "DEL SEÑOR ES LA TIERRA,";
+        let Asamblea2 = "Y TODO AQUELLO QUE CONTIENE,";
+        let Asamblea3 = "ALZAOS, PUERTAS,";
+        let Asamblea4 = "ALZAD LOS DINTELES,";
+        let Asamblea5 = "PUERTAS ETERNAS,";
+        let Asamblea6 = "DEBE ENTRAR EL REY";
+        let Asamblea7 = "DE LA GLORIA, DE LA GLORIA.";
+        let Asamblea8 = "¿QUIÉN ES ESE REY DE LA GLORIA?";
+        let Asamblea9 = "EL SEÑOR, EL FUERTE, EL VALIENTE,";
+        let Asamblea10 = "VALIENTE EN LA BATALLA.";
+        let Asamblea11 = "EL SEÑOR DE LOS EJÉRCITOS.";
+        let Asamblea12 = "EL REY DE LA GLORIA.";
+        let Asamblea13 = "ALZAOS, PUERTAS,";
+        let Asamblea14 = "ALZAD LOS DINTELES,";
+        let Asamblea15 = "PUERTAS ETERNAS,";
+        let Asamblea16 = "DEBE ENTRAR EL REY";
+        let Asamblea17 = "DE LA GLORIA, DE LA GLORIA.";
+        let Asamblea18 = "¿QUIÉN SUBIRÁ AL MONTE DEL SEÑOR?";
+        let Asamblea19 = "EL DE MANOS INOCENTES";
+        let Asamblea20 = "Y PURO CORAZÓN,";
+        let Asamblea21 = "POR VANIDAD.";
+        let Asamblea22 = "Y NOSOTROS SOMOS SU IGLESIA,";
+        let Asamblea23 = "QUE HOY VIENE A TU PRESENCIA,";
+        let Asamblea24 = "OH DIOS DE JACOB.";
+        let Asamblea25 = "ALZAOS, PUERTAS,";
+        let Asamblea26 = "ALZAD LOS DINTELES,";
+        let Asamblea27 = "PUERTAS ETERNAS,";
+        let Asamblea28 = "DEBE ENTRAR EL REY";
+        let Asamblea29 = "DE LA GLORIA, DE LA GLORIA.";
+
+        // Definir variables con la parte de la Asamble	
+        let Cantor1 = "Del Señor es la tierra,";
+        let Cantor2 = "y todo aquello que contiene,";
+        let Cantor3 = "el orbe y sus habitantes.";
+        let Cantor4 = "¿Quién es ese Rey de la gloria?";
+        let Cantor5 = "El Señor, el fuerte, el valiente,";
+        let Cantor6 = "el Señor, valiente en la batalla,";
+        let Cantor7 = "El Señor de los ejércitos,";
+        let Cantor8 = "Él es el Rey de la gloria,";
+        let Cantor9 = "¿Quién subirá al monte del Señor?";
+        let Cantor10 = "El de manos inocentes y puro corazón,";
+        let Cantor11 = "el que no conduce su vida por vanidad,";
+        let Cantor12 = "Y nosotros somos su Iglesia,";
+        let Cantor13 = "que hoy viene a tu presencia,";
+        let Cantor14 = "que busca tu rostro, oh Dios de Jacob,";
+
+
+        // Asignando variable Tituto//	
+        document.getElementById("t1").textContent = titulo;
+        document.getElementById("s1").textContent = salmo;
+        document.getElementById("dbno").textContent = dbnos;
+
+        // Asignando variable para Asamble//	
+        document.getElementById("a1").textContent = Asamblea1;
+        document.getElementById("a2").textContent = Asamblea2;
+        document.getElementById("a3").textContent = Asamblea3;
+        document.getElementById("a4").textContent = Asamblea4;
+        document.getElementById("a5").textContent = Asamblea5;
+        document.getElementById("a6").textContent = Asamblea6;
+        document.getElementById("a7").textContent = Asamblea7;
+        document.getElementById("a8").textContent = Asamblea8;
+        document.getElementById("a9").textContent = Asamblea9;
+        document.getElementById("a10").textContent = Asamblea10;
+        document.getElementById("a11").textContent = Asamblea11;
+        document.getElementById("a12").textContent = Asamblea12;
+        document.getElementById("a13").textContent = Asamblea13;
+        document.getElementById("a14").textContent = Asamblea14;
+        document.getElementById("a15").textContent = Asamblea15;
+        document.getElementById("a16").textContent = Asamblea16;
+        document.getElementById("a17").textContent = Asamblea17;
+        document.getElementById("a18").textContent = Asamblea18;
+        document.getElementById("a19").textContent = Asamblea19;
+        document.getElementById("a20").textContent = Asamblea20;
+        document.getElementById("a21").textContent = Asamblea21;
+        document.getElementById("a22").textContent = Asamblea22;
+        document.getElementById("a23").textContent = Asamblea23;
+        document.getElementById("a24").textContent = Asamblea24;
+        document.getElementById("a25").textContent = Asamblea25;
+        document.getElementById("a26").textContent = Asamblea26;
+        document.getElementById("a27").textContent = Asamblea27;
+        document.getElementById("a28").textContent = Asamblea28;
+        document.getElementById("a29").textContent = Asamblea29;
+
+        // Asignando variable para Cantor o Solista//	
+        document.getElementById("c1").textContent = Cantor1;
+        document.getElementById("c2").textContent = Cantor2;
+        document.getElementById("c3").textContent = Cantor3;
+        document.getElementById("c4").textContent = Cantor4;
+        document.getElementById("c5").textContent = Cantor5;
+        document.getElementById("c6").textContent = Cantor6;
+        document.getElementById("c7").textContent = Cantor7;
+        document.getElementById("c8").textContent = Cantor8;
+        document.getElementById("c9").textContent = Cantor9;
+        document.getElementById("c10").textContent = Cantor10;
+        document.getElementById("c11").textContent = Cantor11;
+        document.getElementById("c12").textContent = Cantor12;
+        document.getElementById("c13").textContent = Cantor13;
+        document.getElementById("c14").textContent = Cantor14;
+        // Fin del Codigo //	
