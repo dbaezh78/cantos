f36f3af27e28f16db820bb3d0d25795087fc252a
dbaezh78 <108948897+dbaezh78@users.noreply.github.com>
2025-04-21 13:46:23 -0400

    update

---
+++ src/cjs/amenamenamen.js
@@ -0,0 +1,98 @@
+// Titulo	
+let titulo = "AMÉN, AMÉN, AMÉN ";
+let salmo = "Apocalipsis 7,12-14 ";
+let dbnos = "17";      //Numero del libro
+
+// Definir variables con la parte de la Asamble	
+let Asamblea1 = "AMÉN, AMÉN, AMÉN.";
+let Asamblea2 = "AMÉN, AMÉN, AMÉN.";
+let Asamblea3 = "AMÉN, AMÉN, AMÉN.";
+let Asamblea4 = "BENDICIÓN Y GLORIA,";
+let Asamblea5 = "SABIDURÍA,";
+let Asamblea6 = "ACCIÓN DE GRACIAS";
+let Asamblea7 = "DEMOS A DIOS.";
+let Asamblea8 = "AMÉN, AMÉN, AMÉN.";
+let Asamblea9 = "AMÉN, AMÉN, AMÉN.";
+let Asamblea10 = "AMÉN, AMÉN, AMÉN.";
+let Asamblea11 = "BENDICIÓN Y GLORIA,";
+let Asamblea12 = "SABIDURÍA,";
+let Asamblea13 = "ACCIÓN DE GRACIAS";
+let Asamblea14 = "DEMOS A DIOS.";
+let Asamblea15 = "AMÉN, AMÉN, AMÉN.";
+let Asamblea16 = "AMÉN, AMÉN, AMÉN.";
+let Asamblea17 = "AMÉN, AMÉN, AMÉN.";
+let Asamblea18 = "¿QUIÉNES SON";
+let Asamblea19 = "Y DE DÓNDE VIENEN?";
+let Asamblea20 = "ESTOS SON LOS QUE VIENEN";
+let Asamblea21 = "DE LA GRAN TRIBULACIÓN.";
+let Asamblea22 = "Y LAVARON SUS TÚNICAS";
+let Asamblea23 = "Y LAS BLANQUEARON";
+let Asamblea24 = "EN LA SANGRE DEL CORDERO,";
+let Asamblea25 = "EN LA SANGRE DEL CORDERO. ";
+let Asamblea26 = "ESTOS SON LOS QUE VIENEN";
+let Asamblea27 = "DE LA GRAN TRIBULACIÓN.";
+let Asamblea28 = "Y LAVARON SUS TÚNICAS";
+let Asamblea29 = "Y LAS BLANQUEARON";
+let Asamblea30 = "EN LA SANGRE DEL CORDERO,";
+let Asamblea31 = "EN LA SANGRE DEL CORDERO. ";
+let Asamblea32 = "AMÉN, AMÉN, AMÉN.";
+let Asamblea33 = "AMÉN, AMÉN, AMÉN.";
+let Asamblea34 = "AMÉN, AMÉN, AMÉN.";
+
+// Definir variables con la parte de la Asamble	
+let Cantor1 = "Honor y poder";
+let Cantor2 = "y fortaleza,";
+let Cantor3 = "honor y poder";
+let Cantor4 = "demos a Dios.";
+let Cantor5 = "¿Quiénes son";
+let Cantor6 = "y de dónde vienen?";
+
+// Asignando variable Tituto//	
+document.getElementById("t1").textContent = titulo;
+document.getElementById("s1").textContent = salmo;
+document.getElementById("dbno").textContent = dbnos;
+
+// Asignando variable para Asamble//	
+document.getElementById("a1").textContent = Asamblea1;
+document.getElementById("a2").textContent = Asamblea2;
+document.getElementById("a3").textContent = Asamblea3;
+document.getElementById("a4").textContent = Asamblea4;
+document.getElementById("a5").textContent = Asamblea5;
+document.getElementById("a6").textContent = Asamblea6;
+document.getElementById("a7").textContent = Asamblea7;
+document.getElementById("a8").textContent = Asamblea8;
+document.getElementById("a9").textContent = Asamblea9;
+document.getElementById("a10").textContent = Asamblea10;
+document.getElementById("a11").textContent = Asamblea11;
+document.getElementById("a12").textContent = Asamblea12;
+document.getElementById("a13").textContent = Asamblea13;
+document.getElementById("a14").textContent = Asamblea14;
+document.getElementById("a15").textContent = Asamblea15;
+document.getElementById("a16").textContent = Asamblea16;
+document.getElementById("a17").textContent = Asamblea17;
+document.getElementById("a18").textContent = Asamblea18;
+document.getElementById("a19").textContent = Asamblea19;
+document.getElementById("a20").textContent = Asamblea20;
+document.getElementById("a21").textContent = Asamblea21;
+document.getElementById("a22").textContent = Asamblea22;
+document.getElementById("a23").textContent = Asamblea23;
+document.getElementById("a24").textContent = Asamblea24;
+document.getElementById("a25").textContent = Asamblea25;
+document.getElementById("a26").textContent = Asamblea26;
+document.getElementById("a27").textContent = Asamblea27;
+document.getElementById("a28").textContent = Asamblea28;
+document.getElementById("a29").textContent = Asamblea29;
+document.getElementById("a30").textContent = Asamblea30;
+document.getElementById("a31").textContent = Asamblea31;
+document.getElementById("a32").textContent = Asamblea32;
+document.getElementById("a33").textContent = Asamblea33;
+document.getElementById("a34").textContent = Asamblea34;
+
+// Asignando variable para Cantor o Solista//	
+document.getElementById("c1").textContent = Cantor1;
+document.getElementById("c2").textContent = Cantor2;
+document.getElementById("c3").textContent = Cantor3;
+document.getElementById("c4").textContent = Cantor4;
+document.getElementById("c5").textContent = Cantor5;
+document.getElementById("c6").textContent = Cantor6;
+// Fin del Codigo //
